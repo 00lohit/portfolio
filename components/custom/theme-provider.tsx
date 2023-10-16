ae67bad85d38bdc86eaae5045cc460f6edc7ad89
Lohit <00lohit@gmail.com>
2023-10-16 17:52:02 +0530

    use effect fix

--- components/custom/theme-provider.tsx
+++ components/custom/theme-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import { useEffect } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
@@ -22,9 +22,9 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
-  React.useEffect(() => {
+  useEffect(() => {
     setTheme("system");
-  }, []);
+  });
 
   return (
     <DropdownMenu>
