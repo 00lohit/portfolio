7c822d0ca585b16b677f1c2b0149727a16d831da
Lohit <00lohit@gmail.com>
2023-10-25 00:39:17 +0530

    update

--- app/Content.tsx
+++ app/Content.tsx
@@ -130,13 +130,18 @@ const ExtraContent = () => (
       { img: bal, link: "https://www.youtube.com/watch?v=HjPgdhRsGIQ" },
       { img: gen, link: "https://www.youtube.com/watch?v=R8vlNbk0Yww" },
     ].map(({ img, link }, i) => (
-      <a target="_blank" href={link} key={i.toString()}>
+      <a
+        target="_blank"
+        href={link}
+        key={i.toString()}
+        className="w-1/2 md:w-full h-auto"
+      >
         <Image
           src={img}
           layout={"responsive"}
-          alt={`A cute animal!`}
-          // unoptimized={true}
-          className="w-1/2 md:w-full h-auto scale-95 md:hover:scale-100 transition-transform duration-300 ease-in-out"
+          alt={`bla bala bala!!`}
+          unoptimized={true}
+          className="scale-95 md:hover:scale-100 transition-transform duration-300 ease-in-out"
         />
       </a>
     ))}
