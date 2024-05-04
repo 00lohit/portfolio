6ec1e83f0e33af52695229304d0b732c37093548
lohit-turtil <lohit.prakash@turtil.co>
2024-05-04 19:01:19 +0530

    re route

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AlertProvider } from "@/components/custom/show-alert";
+import MetaTags from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <MetaTags>
+          <meta name="apple-itunes-app" content="app-id=6498628142" />
+        </MetaTags>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <AlertProvider>{children}</AlertProvider>
